9fd9520c9f27ed77336389ffff21d188f5031244
CaiquevFreitas <caique.v.freitas@ba.estudante.senai.br>
2024-09-23 20:20:31 -0300

    Versão 2.0
    Todas as teclas estão funcionando, foi incluído as teclas de limpar e de ponto. Removi as teclas de potência e de porcentagem

--- Calculadora/main.js
+++ Calculadora/main.js
@@ -1,5 +1,44 @@
 let visor = ""
-function pegarValores(button){
-    visor  += button;
-    document.getElementById("visor").innerHTML += button;
+let parents = true;
+function principal(button){
+    limpar(button);
+    mostrarValores(button);
+    parenteses(button);
+    calcular(button);
+}
+
+function mostrarValores(button){
+    if(button != "C" && button != "=" && button != "p"){
+        visor  += button;
+        document.getElementById("visor").innerHTML += button;
+    }
+}
+
+function limpar(button){
+    if(button == "C"){
+        document.getElementById("visor").innerHTML = "";
+        visor = "";
+    }
+}
+
+function calcular(button){
+    if(button == "="){
+        result = eval(visor); //transforma o texto em uma expressão matematica
+        document.getElementById("visor").innerHTML = result;
+    }
+}
+
+function parenteses(button){
+    if(button == "p"){
+        if(parents){
+        visor += "(";
+        document.getElementById("visor").innerHTML += "(";
+        parents = !parents;
+        }
+        else{
+            visor += ")";
+            document.getElementById("visor").innerHTML += ")";
+            parents = !parents;
+        }
+    }
 }
