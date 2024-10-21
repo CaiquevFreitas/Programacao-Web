402950105bb13642f0ead87de00ab928ab5361af
CaiquevFreitas <caique.v.freitas@ba.estudante.senai.br>
2024-10-20 21:18:06 -0300

    Versão 1.0
    Inicio do projeto do site de animes

---
+++ Animes/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));//permite que o navegador acesse as imagens
+
+app.get('/',(req,res)=>{
+    res.render('inicio');
+})
+
+app.get('/lista',(req,res)=>{
+    res.render('listaAnimes');
+})
+
+
+
+
+app.listen(port,()=>{
+    console.log(`Servidor rodando: localhost:${port}`);
+});
